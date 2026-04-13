9e540d6a8bfb717372cbecec36600d87150f053f
Denis Potapov <DenisPotapov-0@yandex.ru>
2026-04-13 23:23:44 +0300

    rewrite tests to СUnit and add CI

--- test/test_Galois_Field_256.c
+++ test/test_Galois_Field_256.c
@@ -1,111 +1,135 @@
-
 #include "Galois_Field_256.h"
-#include <assert.h>
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 #include <stdio.h>
 
-// if it is a Field:
-// 1) a + b = b + a
-// 2) (a + b) + c = a + (b + c)
-// 3) a + 0 = a
-// 4) a + (-a) = 0
-// 5) a * b = b * a
-// 6) (a * b) * c = a * (b * c)
-// 7) a * 1 = a
-// 8) a * (a**(-1)) = 1
-// 9) (a + b) * c = a * c + b * c
+#define CU_ASSERT_MSG(expr, fmt, ...)
+do {
+    if (!(expr)) {
+        char _msg[512];
+        snprintf(_msg, sizeof(_msg), fmt, ##__VA_ARGS__);
+        CU_FAIL(_msg);
+    }
+} while (0)
 
-void test_1(void) {
+    void
+    test_add_commutative(void) {
     for (uint16_t a = 0; a < 256; a++) {
         for (uint16_t b = 0; b < 256; b++) {
-            assert(GF256_Add(a, b) == GF256_Add(b, a));
+            CU_ASSERT_MSG(GF256_Add(a, b) == GF256_Add(b, a),
+                          "GF256_Add(%u,%u) != GF256_Add(%u,%u)", a, b, b, a);
         }
     }
-    printf("1 test passed!\n");
 }
 
-void test_2(void) {
-    for (uint16_t a = 0; a < 256; a++) {
-        for (uint16_t b = 0; b < 256; b++) {
-            for (uint16_t c = 0; c < 256; c++) {
-                assert(GF256_Add(GF256_Add(a, b), c) == GF256_Add(a, GF256_Add(b, c)));
-            }
-        }
-    }
-    printf("2 test passed!\n");
-}
-
-void test_3(void) {
-    for (uint16_t a = 0; a < 256; a++) {
-        assert(GF256_Add(a, 0) == a);
-    }
-    printf("3 test passed!\n");
-}
-
-void test_4(void) {
-    for (uint16_t a = 0; a < 256; a++) {
-        assert(GF256_Add(a, GF256_Sub(0, a)) == 0);
-    }
-    printf("4 test passed!\n");
-}
-
-void test_5(void) {
-    for (uint16_t a = 0; a < 256; a++) {
-        for (uint16_t b = 0; b < 256; b++) {
-            assert(GF256_Mul(a, b) == GF256_Mul(b, a));
-        }
-    }
-    printf("5 test passed!\n");
-}
-
-void test_6(void) {
+void test_add_associative(void) {
     for (uint16_t a = 0; a < 256; a++) {
         for (uint16_t b = 0; b < 256; b++) {
             for (uint16_t c = 0; c < 256; c++) {
-                assert(GF256_Mul(GF256_Mul(a, b), c) == GF256_Mul(a, GF256_Mul(b, c)));
+                uint8_t left  = GF256_Add(GF256_Add(a, b), c);
+                uint8_t right = GF256_Add(a, GF256_Add(b, c));
+                CU_ASSERT_MSG(left == right, "Associativity failed for (%u+%u)+%u", a, b, c);
             }
         }
     }
-    printf("6 test passed!\n");
 }
 
-void test_7(void) {
+void test_add_neutral_elem(void) {
     for (uint16_t a = 0; a < 256; a++) {
-        assert(GF256_Mul(a, 1) == a);
+        CU_ASSERT_MSG(GF256_Add(a, 0) == a, "GF256_Add(%u,0) != %u", a, a);
     }
-    printf("7 test passed!\n");
 }
 
-void test_8(void) {
+void test_add_inverse(void) {
+    for (uint16_t a = 0; a < 256; a++) {
+        uint8_t inv = GF256_Sub(0, a);
+        CU_ASSERT_MSG(GF256_Add(a, inv) == 0, "GF256_Add(%u, %u) != 0", a, inv);
+    }
+}
+
+void test_mul_commutative(void) {
+    for (uint16_t a = 0; a < 256; a++) {
+        for (uint16_t b = 0; b < 256; b++) {
+            CU_ASSERT_MSG(GF256_Mul(a, b) == GF256_Mul(b, a),
+                          "GF256_Mul(%u,%u) != GF256_Mul(%u,%u)", a, b, b, a);
+        }
+    }
+}
+
+void test_mul_associative(void) {
+    for (uint16_t a = 0; a < 256; a++) {
+        for (uint16_t b = 0; b < 256; b++) {
+            for (uint16_t c = 0; c < 256; c++) {
+                uint8_t left  = GF256_Mul(GF256_Mul(a, b), c);
+                uint8_t right = GF256_Mul(a, GF256_Mul(b, c));
+                CU_ASSERT_MSG(left == right, "Associativity failed for (%u*%u)*%u", a, b, c);
+            }
+        }
+    }
+}
+
+void test_mul_neutral_elem(void) {
+    for (uint16_t a = 0; a < 256; a++) {
+        CU_ASSERT_MSG(GF256_Mul(a, 1) == a, "GF256_Mul(%u,1) != %u", a, a);
+    }
+}
+
+void test_mul_inverse(void) {
     for (uint16_t a = 1; a < 256; a++) {
-        assert(GF256_Mul(a, GF256_Div(1, a)) == 1);
+        uint8_t inv = GF256_Div(1, a);
+        CU_ASSERT_MSG(GF256_Mul(a, inv) == 1, "GF256_Mul(%u, inv) != 1 (inv=%u)", a, inv);
     }
-    printf("8 test passed!\n");
 }
 
-void test_9(void) {
+void test_distributive(void) {
     for (uint16_t a = 0; a < 256; a++) {
         for (uint16_t b = 0; b < 256; b++) {
             for (uint16_t c = 0; c < 256; c++) {
-                GF256_t res1, res2;
-                res1 = GF256_Mul(GF256_Add(a, b), c);
-                res2 = GF256_Add(GF256_Mul(a, c), GF256_Mul(b, c));
-                assert(res1 == res2);
+                uint8_t left  = GF256_Mul(GF256_Add(a, b), c);
+                uint8_t right = GF256_Add(GF256_Mul(a, c), GF256_Mul(b, c));
+                CU_ASSERT_MSG(left == right, "Distributive law failed for (%u+%u)*%u", a, b, c);
             }
         }
     }
-    printf("9 test passed!\n");
+}
+
+void test_div_consistency(void) {
+    for (uint16_t a = 0; a < 256; a++) {
+        for (uint16_t b = 1; b < 256; b++) {
+            uint8_t q = GF256_Div(a, b);
+            CU_ASSERT_MSG(GF256_Mul(q, b) == a, "GF256_Div(%u,%u) inconsistent: %u * %u != %u", a,
+                          b, q, b, a);
+        }
+    }
 }
 
 int main(void) {
-    test_1();
-    test_2();
-    test_3();
-    test_4();
-    test_5();
-    test_6();
-    test_7();
-    test_8();
-    test_9();
+    if (CU_initialize_registry() != CUE_SUCCESS) {
+        return CU_get_error();
+    }
 
-    return 0;
+    CU_pSuite pSuite = CU_add_suite("GF(256) Field Axioms", NULL, NULL);
+    if (!pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    CU_add_test(pSuite, "test_add_commutative", test_add_commutative);
+    CU_add_test(pSuite, "test_add_associative", test_add_associative);
+    CU_add_test(pSuite, "test_add_neutral_elem", test_add_neutral_elem);
+    CU_add_test(pSuite, "test_add_inverse", test_add_inverse);
+    CU_add_test(pSuite, "test_mul_commutative", test_mul_commutative);
+    CU_add_test(pSuite, "test_mul_associative", test_mul_associative);
+    CU_add_test(pSuite, "test_mul_neutral_elem", test_mul_neutral_elem);
+    CU_add_test(pSuite, "test_mul_inverse", test_mul_inverse);
+    CU_add_test(pSuite, "test_distributive", test_distributive);
+    CU_add_test(pSuite, "test_div_consistency", test_div_consistency);
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+
+    unsigned int num_failures = CU_get_number_of_failures();
+    CU_cleanup_registry();
+
+    return (num_failures == 0) ? 0 : 1;
 }
